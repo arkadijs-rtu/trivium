4fbbb1e8f281b31b6a2d6fdf5325bb8a58f7bc1a
arkade <arkadeee@boss.com>
2023-05-12 10:10:21 +1400

    Fix: writing key data

--- TriviumCipher/Program.cs
+++ TriviumCipher/Program.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace TriviumCipher
 {
     class Program
     {
+        
         static Random rnd = new Random();
         static void Main(string[] args)
         {
-
             byte[] key = new byte[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             byte[] iv = new byte[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
+            //Generate random IV
             for(int i =0;i < iv.Length; i++)
             {
                 iv[i] = (byte)rnd.Next(255);
             }
-
+            
             Console.Write("Input key (max 10 characters): ");
             string skey = Console.ReadLine();
 
@@ -30,6 +32,7 @@ namespace TriviumCipher
 
             for(int i =0;i < key.Length;i++)
             {
+                if (i >= keyb.Length) break;
                 key[i] = keyb[i];
             }
 
@@ -56,7 +59,6 @@ namespace TriviumCipher
             Console.WriteLine("[!] Decryption result: {0}", result);
 
             Console.ReadKey();
-
         }
 
         static string printBytes(byte[] bytes)
